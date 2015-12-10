d435e5f0a8c1d27a6664a703d6c0dd09654d4fa6
Nathan Toone <nathan@toonetown.com>
2015-12-10 14:14:20 -0700

    Use bootstrap-sass instead of the less-based bootstrap

--- src/kit/js/app.js
+++ src/kit/js/app.js
@@ -1,6 +1,6 @@
 /* jQuery and Bootstrap */
 //@codekit-prepend "../../bower_components/jquery/dist/jquery.js"
-//@codekit-prepend "../../bower_components/bootstrap/dist/js/bootstrap.js"
+//@codekit-prepend "../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"
 
 /* IE 10 viewport hack - see http://getbootstrap.com/getting-started/#support-ie10-width */
 //@codekit-prepend "_ie-10-viewport-hack.js"
